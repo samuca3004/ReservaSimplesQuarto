2b7d7f8b05e6df775663539676e7e1aae4134d41
Samuel Ribeiro <samuca3004@hotmail.com>
2022-02-25 22:10:18 -0300

    Add project files.

---
+++ ConsoleApp18/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp18
+{
+    internal class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public int QuarInd { get; set; }
+
+        public Cliente(string nome, string email, int quarInd)
+        {
+            Nome = nome;
+            Email = email;
+            QuarInd = quarInd;
+        }
+
+        public override string ToString()
+        {
+            return QuarInd + ": " + Nome + ", " + Email;
+        }
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// Reserva de quartos utilizando array
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos Quartos serão alugados: ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Cliente[] alugado = new Cliente[quartos];
+
+            for (int i = 0; i < quartos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Aluguel #" + (i + 1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Numero do quarto: ");
+                int quart = int.Parse(Console.ReadLine());
+                alugado[i] = new Cliente(nome, email, quart);
+
+
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados");
+            for (int i = 0; i < quartos; i++)
+            {
+
+                Console.WriteLine(alugado[i]);
+
+            }
+
+        }
+    }
+}
